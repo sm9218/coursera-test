0851de8e31aaf5972324a4d16844b24114a19c8a
sm9218 <imtl9218@gmail.com>
2016-07-16 23:31:17 +0300

    Mod4

---
+++ module4-solution/script.js
@@ -0,0 +1,16 @@
+// *******************************
+// START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
+// *******************************
+(function (){
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+  if (firstLetter == "j") { 
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
+})();
+
+
